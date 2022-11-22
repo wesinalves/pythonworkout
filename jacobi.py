3312cc526d2dd589e352b567eeb76bc837f25b82
Wesin Ribeiro <wesinec@gmail.com>
2022-11-22 19:03:25 -0300

    update jacobi

--- jacobi.py
+++ jacobi.py
@@ -19,7 +19,7 @@ by Wesin Ribeiro
 
 def is_close(vetx, vety):
     close = False
-    n = len(vetx)    
+    n = len(vetx)
     for i in range(n):
         if abs(vetx[i] - vety[i]) < 0.0001:
             close = True
@@ -31,37 +31,44 @@ def is_close(vetx, vety):
 def jacobi_method(A, b, x, n, max_iteration):
     x_new = [0]*len(x)
 
-    for _ in range(max_iteration):    
+    for _ in range(max_iteration):
         for i in range(n):
             sig = 0
             for j in range(n):
                 if j != i:
                     sig = sig + A[i][j] * x[j]
-            x_new[i] = (b[i] - sig) / A[i][i]   
-        
+            
+            x_new[i] = (b[i] - sig) / A[i][i]
+
         if is_close(x, x_new):
             break
 
         x = x_new.copy()
-    
+
     return x
 
-
 def main():
-    A = [[10, -1, 2, 0], 
-        [-1, 11, -1, 3], 
-        [2, -1, 10, -1], 
-        [0, 3, -1, 8]]
+    A = [
+        [10, 1, 2, 0],
+        [-1, 11, -1, 3],
+        [2, -1, 10, -1],
+        [0, 3, -1, 8]
+    ]
     b = [6, 25, -11, 15]
     x0 = [0, 0, 0, 0]
-    
+
     max_iteration = 100
 
-    n = len(A[0])
+    n = len(b)
     x = jacobi_method(A,b,x0,n,max_iteration)
     print(x)
-
+    
 main()
+
+
+
+
+
 
 
 
